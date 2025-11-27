d0f99625ce9231b7b76289bcedb584a07742d4df
goutham1220 <goutham1220@gmail.com>
2025-11-27 16:31:21 -0500

    added cloudbuild.yaml and fixed authentication flow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,21 +38,21 @@ export default function RootLayout({
           colorPrimary: "#1DB954", // Spotify green
         }
       }}
-      domain="https://spotifyshopcentral.app"
+      domain="spotifyshopcentral.app"
       isSatellite={false}
     >
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
-        suppressHydrationWarning
-      >
-        <Navbar />
-        <main className="flex-1">
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
+          suppressHydrationWarning
+        >
+          <Navbar />
+          <main className="flex-1">
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
